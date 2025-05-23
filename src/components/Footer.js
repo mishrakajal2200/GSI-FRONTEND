dcf6b5dbaab28780e85f698d3c309448d88f7d9b
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-05-23 11:36:12 -0500

    changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaWhatsapp, FaCcVisa, FaCcMastercard } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+
 
 function Footer() {
   return (
