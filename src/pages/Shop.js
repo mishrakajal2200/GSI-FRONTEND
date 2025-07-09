b015001e47c641bd2f5b89abdb8937dbd06f7b2c
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-07-09 16:02:28 -0500

    snfkdsf

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -232,7 +232,7 @@ const Shop = () => {
     <div className="bg-gradient-to-br from-purple-300 via-pink-300 to-yellow-300 py-10">
       <ToastContainer />
       <div className="container mx-auto px-4 md:px-8 lg:px-12">
-        <h1 className="text-3xl font-bold text-gray-800 mb-8 text-center">Shops Our Products</h1>
+        <h1 className="text-3xl font-bold text-gray-800 mb-8 text-center">Shop Our Products</h1>
         {/* Conditionally render the sidebar on non-mobile screens */}
           {!isMobile && (
             <aside className="md:w-1/4 bg-gradient-to-br from-gray-800 via-gray-900 to-black p-6 rounded-xl shadow-lg text-white flex-shrink-0">
