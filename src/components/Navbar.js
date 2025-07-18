02338b768042a9ca76f8b213384e4ef5ae005063
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-07-18 06:34:24 -0500

    lskfsdl

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,9 +19,9 @@ import { useWishlist } from "../context/WishlistContext.js";
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  
+  const [user, setUser] = useState(null);
   const navigate = useNavigate();
-  const { cart,user,  setUser } = useCart();
+  const { cart} = useCart();
   const { wishlist } = useWishlist();
 
   const toggleNav = () => setIsNavOpen(!isNavOpen);
@@ -45,24 +45,18 @@ const Navbar = () => {
 
   //   navigate("/login");
   // };
-// const handleLogout = () => {
-//   console.log("Logged out");
-//   localStorage.removeItem("token");
-//   setUser(null); // This will cause Navbar to re-render and show Login/Signup
-//   navigate("/login");
-// };
 const handleLogout = () => {
   console.log("Logged out");
   localStorage.removeItem("token");
-  localStorage.removeItem("user");
-  setUser(null);
+ setUser(null);
   navigate("/login");
 };
 
+
 useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    setUser(storedUser ? JSON.parse(storedUser) : null); // ✅ No error now
-  }, []);
+  const storedUser = localStorage.getItem("user");
+  setUser(storedUser ? JSON.parse(storedUser) : null);
+}, [setUser]);
 
   const handleSearch = async (e) => {
   e.preventDefault();
