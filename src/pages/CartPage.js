cf35791e6ef40b182f60896c48b34c7c16b2fda5
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-08-29 07:13:42 -0500

    lsdfl

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -52,7 +52,7 @@ const CartPage = () => {
   const handleQuotationSubmit = async () => {
   try {
     await axios.post(
-      "https://api.gsienterprises.com/api/quotation/create",
+      "https://api.gsienterprises.com/api/quotations/create",
       {
         items: cart.map((item) => ({
           productId: item.product._id,
