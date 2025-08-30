91249e877e97c3ad07f97fc00d15d0302967bf78
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-08-30 04:16:48 -0500

    kfdl

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -75,7 +75,7 @@ const handleQuotationSubmit = async () => {
     const token = localStorage.getItem("token"); // Or sessionStorage
 
     await axios.post(
-      "https://api.gsienterprises.com/api/quotation/create",
+      "https://api.gsienterprises.com/api/cart/quotation/create",
       {
         items: cart.map((item) => ({
           productId: item.product._id,
