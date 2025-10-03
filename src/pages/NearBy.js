4c741b1cc5a2e11096991a9bb6cb35cf130fe7e3
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2025-10-03 07:41:22 -0500

    sdfl

--- src/pages/NearBy.js
+++ src/pages/NearBy.js
@@ -6,7 +6,7 @@ const NearBy = () => {
   useEffect(() => {
     const fetchShops = async () => {
       try {
-        const res = await fetch(`/nearby/shops`,{withCredentials:true});
+        const res = await fetch(`/api/nearby/shops`,{withCredentials:true});
         const data = await res.json();
         setShops(data);
       } catch (err) {
